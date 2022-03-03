2bfde0bad80b7bc66f0681ffdfc9c31a7ef41df6
Karolyne Corol <karolyne.corol@gmail.com>
2022-03-03 14:31:23 -0300

    @KarolyneCorol [TASK: corrigindo service]

--- src/main/java/com/blog/pessoal/blogpessoal/Controller/UserController.java
+++ src/main/java/com/blog/pessoal/blogpessoal/Controller/UserController.java
@@ -1,19 +1,28 @@
 package com.blog.pessoal.blogpessoal.Controller;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import com.blog.pessoal.blogpessoal.Model.Login;
 import com.blog.pessoal.blogpessoal.Model.User;
+import com.blog.pessoal.blogpessoal.Repository.UserRepository;
 import com.blog.pessoal.blogpessoal.Service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/user")
@@ -21,18 +30,51 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
     
-    @Autowired
-    private UsuarioService usuarioService;
-
-    @PostMapping("/logar")
-    public ResponseEntity<Login> Autentication(@RequestBody Optional<Login> usuario){
-		return usuarioService.Logar(usuario).map(resp -> ResponseEntity.ok(resp))
-			.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    
+	private @Autowired UsuarioService usuarioService;
+	
+	private @Autowired UserRepository userRepository;
+    
+	@GetMapping("/all")
+	public ResponseEntity <List<User>> getAll() {
+		return ResponseEntity.ok(userRepository.findAll());
 	}
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<User> Post(@RequestBody User usuario) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-        .body(usuarioService.CadastrarUsuario(usuario));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(@PathVariable long id) {
+		return userRepository.findById(id)
+			.map(resp -> ResponseEntity.ok(resp))
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/{nome}")
+	public ResponseEntity<List<User>> getByUsuarios (@PathVariable String nome)  {
+		return ResponseEntity.ok(userRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<Login> authentication(@RequestBody Optional<Login> user) {
+		return usuarioService.logarUsuario(user)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<User> postUsuario(@Valid @RequestBody User user) {
+		return usuarioService.cadastrarUsuario(user)
+				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(resp))
+				.orElse(ResponseEntity.status(400).build());
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<User> putUsuario(@Valid @RequestBody User user){
+		return usuarioService.atualizarUsuario(user)
+			.map(resp -> ResponseEntity.status(HttpStatus.OK).body(resp))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteUsuarios (@PathVariable long id) {
+		userRepository.deleteById(id);
+	}
 }
--- src/main/java/com/blog/pessoal/blogpessoal/Service/UsuarioService.java
+++ src/main/java/com/blog/pessoal/blogpessoal/Service/UsuarioService.java
@@ -9,51 +9,87 @@ import com.blog.pessoal.blogpessoal.Repository.UserRepository;
 
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Implementação do UsuarioService com os métodos relacionados com a model Usuario
+ * 
+ * @author Karolyne Corol
+ * @date 03/03/2022
+ * @version 1.0
+ * 
+ */
 
 @Service
 public class UsuarioService {
 
-	@Autowired
-	private UserRepository repository;
-	
-	public User CadastrarUsuario(User usuario) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
-	
-	return repository.save(usuario);
+	private @Autowired UserRepository repository;
+
+	//Cadastrar Usuário
+	public Optional<User> cadastrarUsuario(User user) {
+
+		if (repository.findByUsuario(user.getUsuario()).isPresent())
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+			user.setSenha(criptografarSenha(user.getSenha()));
+
+		return Optional.of(repository.save(user));
 	}
 	
-	public Optional<Login> Logar(Optional<Login> user) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		Optional<User> usuario = repository.findByUsuario(user.get().getUsuario());
-		
-		if(usuario.isPresent()) {
-			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
-		
-				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
-				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader = "Basic " + new String(encodedAuth);
-				
-				user.get().setToken(authHeader);
-				user.get().setId(user.get().getId());
-				user.get().setNome(user.get().getNome());
-				user.get().setFoto(user.get().getFoto());
-				user.get().setToken(generatorBasicToken(user.get().getUsuario(),user.get().getSenha()));
-				user.get().setSenha(user.get().getSenha());
-				user.get().setTipo(user.get().getTipo());
-				
-				return user;
+	//Atualizar Usuário
+	public Optional<User> atualizarUsuario(User user) {
+		if (repository.findById(user.getId()).isPresent()) {
+			Optional<User> buscaUsuario = repository.findByUsuario(user.getUsuario());
+			if (buscaUsuario.isPresent()) {
+				if (buscaUsuario.get().getId() != user.getId())
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+			}
+			user.setSenha(criptografarSenha(user.getSenha()));
+			return Optional.of(repository.save(user));
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!", null);
+	}
+	
+	//Logar Usuário
+	public Optional<Login> logarUsuario(Optional<Login> login) {
+		Optional<User> user = repository.findByUsuario(login.get().getUsuario());
+
+		if (user.isPresent()) {
+			if (compararSenhas(login.get().getSenha(), user.get().getSenha())) {
+
+				login.get().setId(user.get().getId());
+				login.get().setNome(user.get().getNome());
+				login.get().setFoto(user.get().getFoto());
+				login.get().setToken(generatorBasicToken(login.get().getUsuario(), login.get().getSenha()));
+				login.get().setSenha(user.get().getSenha());
+				login.get().setTipo(user.get().getTipo());
+
+				return login;
 			}
 		}
-		return null;
-	}
-
-	private String generatorBasicToken(String usuario, String senha) {
-		return null;
+		throw new ResponseStatusException(
+			HttpStatus.UNAUTHORIZED, "Usuário ou senha inválidos!", null);
 	}
 	
+	//Criptografar Senha
+	private String criptografarSenha(String senha) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String senhaEncoder = encoder.encode(senha);
+		return senhaEncoder;
+	}
+	
+	//Comparar Senhas
+	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.matches(senhaDigitada, senhaBanco);
+	}
+
+	//Gerador de token
+	private String generatorBasicToken(String usuario, String password) {
+		String structure = usuario + ":" + password;
+		byte[] structureBase64 = Base64.encodeBase64(structure.getBytes(Charset.forName("US-ASCII")));
+		return "Basic " + new String(structureBase64);
+	}
 }
