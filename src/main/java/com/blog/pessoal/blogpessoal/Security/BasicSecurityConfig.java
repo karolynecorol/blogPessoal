a4b35528488151d755f3fe4bd71e626c0af1255e
Karolyne Corol <karolyne.corol@gmail.com>
2022-03-03 14:40:11 -0300

    @KarolyneCorol [corrigindo bug]

--- src/main/java/com/blog/pessoal/blogpessoal/Security/BasicSecurityConfig.java
+++ src/main/java/com/blog/pessoal/blogpessoal/Security/BasicSecurityConfig.java
@@ -19,8 +19,8 @@ public class BasicSecurityConfig extends  WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService; 
 
     @Override  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-           auth.userDetailsService(userDetailsService);
-           auth.inMemoryAuthentication()
+        auth.userDetailsService(userDetailsService);
+        auth.inMemoryAuthentication()
 			.withUser("admin@email.com")
 			.password(passwordEncoder().encode("admin"))
 			.authorities("ROLE_ADMIN");
@@ -34,9 +34,8 @@ public class BasicSecurityConfig extends  WebSecurityConfigurerAdapter {
     @Override  
     protected void configure(HttpSecurity http) throws Exception {   
         http.authorizeRequests()
-        .antMatchers("/usuarios/logar").permitAll()
-        .antMatchers("/usuarios/cadastrar").permitAll() 
-        .antMatchers(HttpMethod.GET, "/produtos").permitAll()
+        .antMatchers("/user/logar").permitAll()
+        .antMatchers("/user/cadastrar").permitAll() 
         .anyRequest().authenticated()
         .and().httpBasic()
         .and().sessionManagement()
